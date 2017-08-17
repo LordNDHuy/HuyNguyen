e7afeb503b93572626fc616f2007c993ff42cb27
HuyNguyen01 <lordndhuy@gmail.com>
2017-08-17 21:44:12 +0700

    Add files via upload

---
+++ source/account_management.h
@@ -0,0 +1,55 @@
+#ifndef ACCOUNT_MANAGEMENT_H
+#define ACCOUNT_MANAGEMENT_H
+
+#include <QMainWindow>
+
+#include "include/Account.h"
+#include "include/UsrInfo.h"
+#include <QString>
+typedef struct{
+    QStringList User_name;
+    std::vector<unsigned int> Uid;
+}QS_user;
+typedef struct{
+    std::string accname;
+    unsigned int Aid;
+}QS_acc;
+namespace Ui {
+class Account_management;
+}
+
+class Account_management : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit Account_management(QWidget *parent = 0);
+    ~Account_management();
+    void get_username();
+    void update_remove();
+private slots:
+    void on_option_currentIndexChanged(int index);
+    void on_Register_clicked();
+
+    void on_occu_returnPressed();
+
+    void on_Username_list_currentTextChanged(const QString &currentText);
+
+    void on_Search_Box_textChanged(const QString &arg1);
+
+    void on_add_clicked();
+
+    void on_remove_clicked();
+
+    void get_UID(unsigned int&);
+    void on_Remove_u_clicked();
+
+private:
+    Ui::Account_management *ui;
+    UsersMgr user;
+    AccountsMgr account;
+    QS_user *search_list = new QS_user();
+    std::vector<QS_acc> remove_list;
+};
+
+#endif // ACCOUNT_MANAGEMENT_H
